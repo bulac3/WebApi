df2237ab1ca66f302c4cab2a2f852868bb33e9f2
Alexey.Buloichyk <alexey.buloichyk@effective-soft.com>
2017-07-13 10:26:05 +0300

    Add markers on click

--- WebApi/CoolMap/src/app/app.component.ts
+++ WebApi/CoolMap/src/app/app.component.ts
@@ -1,21 +1,38 @@
 ﻿import { Component, OnInit } from '@angular/core';
 import { Http } from '@angular/http'
+
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.css']
 })
+
 export class AppComponent implements OnInit {
     constructor(private _httpService: Http) { }
-    apiValues: string[] = [];
-    title: string = 'My first AGM project';
-    lat: number = 51.678418;
-    lng: number = 7.809007;
+    values: any;
+    screenLatitude: number;
+    screenLongitude: number;
+    clickLatitude: number;
+    clickLongitude: number;
+    markers: any[] = [
+        {
+            latitude: 51,
+            longitude: 7.06
+        }
+    ]
     ngOnInit() {
         this._httpService.get('http://localhost:60289/api/webapi/').subscribe(values => {
             console.log("Hello1");
-            this.apiValues = values.json() as string[];
-            
+            this.values = values.json();
+            this.screenLatitude = this.values[0].latitude;
+            this.screenLongitude = this. values[0].longitude;
         });
     }
+    mapClicked($event: any) {
+        console.log("Hello1");
+        this.markers.push({
+            latitude: $event.coords.lat,
+            longitude: $event.coords.lng
+        });
+    }   
 }
